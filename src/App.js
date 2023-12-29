a03d66d24bf224d2bc5ba50dbfe777c4c901aad9
daggelito02 <daggelito02@gmail.com>
2023-12-29 18:51:02 +0100

    Visar markers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 //import { GoogleMap, withScriptjs, withGoogleMap} from "@react-google-maps/api";
 import './App.css';
 
@@ -18,23 +18,44 @@ import './App.css';
 
 //export default App;
 
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api';
 
 const libraries = ['places'];
 const mapContainerStyle = {
   width: '70vw',
-  height: '70vh',
+  height: '70vh'
 };
 const center = {
   lat: 55.604980, // default latitude
   lng: 13.003822, // default longitude
 };
+const defaultMarker = {
+  lat: 55.60883, // default latitude
+  lng: 12.99459, // default longitude
+}
+const defaultMarker2 = {
+  lat: 55.60804, // default latitude
+  lng: 13.01062, // default longitude
+}
 
 const App = () => {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: 'AIzaSyDwGQGGoMZbLsSeUIk8tDOegcxBJm-d3fA',
     libraries,
   });
+
+
+//const [markers, setMarkers] = useState([]);
+
+// const onMapClick = (e) => {
+//     setMarkers((current) => [
+//       ...current,
+//       {
+//         lat: e.latLng.lat(),
+//         lng: e.latLng.lng()
+//       }
+//     ]);
+//   };
 
   if (loadError) {
     return <div>Error loading maps</div>;
@@ -50,8 +71,24 @@ const App = () => {
         mapContainerStyle={mapContainerStyle}
         zoom={13.5}
         center={center}
+        options={{ 
+          styles: [
+            { featureType: "poi.business", stylers: [{ visibility: "off"}] },
+            { featureType: "poi.attraction", stylers: [{ visibility: "off"}] },
+            { featureType: "transit.station",stylers:[{visibility:"off"}] }
+          
+          ], }}
+          //onClick={onMapClick}
       >
-        <Marker position={center} />
+        <MarkerF position={defaultMarker} />
+        <MarkerF position={defaultMarker2} label={'Dagge testar'}/>
+        {/* {markers.map((marker) => (
+        <MarkerF
+          position={{ 
+            lat: marker.lat,
+            lng: marker.lng 
+          }} />
+        ))} */}
       </GoogleMap>
     </div>
   );
